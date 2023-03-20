604aadc951b543b1247e80033aea5fa4e5b9392a
Yasmeen-Helmy-Elmandrawy <yasmeenhelmy2020@gmail.com>
2023-03-21 00:58:20 +0200

    AirCover View and Edit Privacy View

--- airbnb/Controllers/HomeController.cs
+++ airbnb/Controllers/HomeController.cs
@@ -27,6 +27,11 @@ namespace airbnb.Controllers
             return View();
         }
 
+        public IActionResult AirCover()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
